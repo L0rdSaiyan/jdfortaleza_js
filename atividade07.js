c89510d094298870c5bcf1d0b02fd3dd8f5263ca
joao victor <thehoodgamer20@gmail.com>
2022-07-31 21:07:48 -0300

    atividade 07

---
+++ atividade07.js
@@ -0,0 +1,15 @@
+function tabuada(){
+
+let numero = window.parseInt(prompt("Informe um número"))
+let resultado = 0;
+for(let i=1; i<=10; i++){
+
+resultado = numero*i
+console.log(resultado)
+
+
+}
+
+
+
+}
