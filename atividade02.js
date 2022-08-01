7e44f30b68800974c7212bdb4cf2720dfe8eab6e
joao victor <thehoodgamer20@gmail.com>
2022-07-31 21:51:45 -0300

    atividade 02

---
+++ atividade02.js
@@ -0,0 +1,22 @@
+function Fibonacci(){
+
+   let soma = 0;
+    let numA = 0;
+    let numB = 0;
+
+for(let i=1; i<=90; i++){
+    
+    numA = i-1
+    numB = i-2
+    soma = numA + numB
+    
+
+
+    document.write(soma)
+
+
+}
+
+
+
+}
